280f1166bc5ba4277f46449957223e30525dc67c
MuLin <276153811@qq.com>
2015-08-14 23:28:55 +0800

    excel python

---
+++ Python_excel/python_excel.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+#============================================================
+#This python used to do excel
+#============================================================
+
+#============================================================
+#config
+#old_key_lines : old line which contain key 
+old_key_lines = 2
+#new_key_lines : new line which contain key 
+new_key_lines = 1
+#old_excel_name 
+old_excel_name = ""
+#new_excel_name
+new_excel_name = ""
+#============================================================
+
+mOld_Key = []
+mOld = []
+mNew_Key = []
+mNew = []
+
+import os
+
+def _ReadExcel(name):
+	#1.判断参数合理性
+	return ()
+
+def main() :
+	#1.read old excel
+	_ReadExcel(old_key_lines)
+	#2.read new excel
+	#3.write msg
+	pass
+
+if __name__ == '__main__':
+	main()
